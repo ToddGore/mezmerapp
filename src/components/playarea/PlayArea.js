7e604c95889cae310ee5abca55e4c930d0eeb278
Todd Gore <toddgore@Todds-MacBook-Pro.local>
2018-06-29 14:48:37 -0600

    Fixed Wrong answer background flasher

--- src/components/playarea/PlayArea.js
+++ src/components/playarea/PlayArea.js
@@ -25,7 +25,8 @@ class PlayArea extends Component {
             respStart: 0,
             respStop: 0,
             respTime: 0,
-            ledTime: true
+            ledTime: true,
+            selElement: ''
 
         };
         this.toggleClass = this.toggleClass.bind(this)
@@ -51,8 +52,9 @@ class PlayArea extends Component {
 
 
     toggleClass() {
-        const currentState = this.state.active;
-        this.setState({ active: !currentState });
+        // const currentState = this.state.active;
+        // this.setState({ active: !currentState });
+        this.setState({ active: true });
         if (!this.state.active) {
             this.displayCard()
         }
@@ -60,8 +62,11 @@ class PlayArea extends Component {
     };
 
     displayCard() {
+        // Gets new values for the card
         let card = this.cardSelector()
         // console.log('cardSel ', card)
+
+        // Set sate card to the new card data, Remove colors from card items
         this.setState({
             card: card,
             ans1: false,
@@ -94,37 +99,45 @@ class PlayArea extends Component {
     // Evaluate answer
     AnswerClickHandler(answer) {
         const card = this.state.card
+
+        switch (answer) {
+
+            case 'answer_1':
+                this.setState({ ans1: true, ans2: false, ans3: false, ans4: false })
+                break;
+            case 'answer_2':
+                this.setState({ ans1: false, ans2: true, ans3: false, ans4: false })
+                break;
+            case 'answer_3':
+                this.setState({ ans1: false, ans2: false, ans3: true, ans4: false })
+                break;
+            case 'answer_4':
+                this.setState({ ans1: false, ans2: false, ans3: false, ans4: true })
+                break;
+            default:
+                break;
+        }
+
+
         // If answer is correct
         if (card.correct_answer === card[answer]) {
 
             this.respTimer('stop')
+            this.setState({
+                selElement: 'box green green:hover'
+            })
 
-            switch (answer) {
-                case 'answer_1':
-                    this.setState({ ans1: true, ans2: false, ans3: false, ans4: false })
-                    break;
-                case 'answer_2':
-
-                    this.setState({ ans1: false, ans2: true, ans3: false, ans4: false })
-                    break;
-                case 'answer_3':
-                    this.setState({ ans1: false, ans2: false, ans3: true, ans4: false })
-                    break;
-                case 'answer_4':
-                    this.setState({ ans1: false, ans2: false, ans3: false, ans4: true })
-                    break;
-                default:
-                    break;
-            }
 
             this.setState({
                 answerState: true,
                 answerText: 'Correct !'
             })
+            // Display the modal
             this.AnswerModal()
+
             setTimeout(() => {
 
-                // Remove Modal
+                // Remove Modal after time delay
                 this.setState({
                     removeModal: true,
                     showModal: false
@@ -134,41 +147,67 @@ class PlayArea extends Component {
                 // Currently being done in Response Timer
 
 
-                // flip card back
-                this.toggleClass()
+                // flip card back after time delay
+                // this.toggleClass()
 
-                // Display a new question
+                // Makes card flip back to "Are you Ready" after delay
+                this.setState({ active: false });
+
+                // Wait, and then flip back to the next question
+                setTimeout(() => {
+                    this.setState({ active: true });
+                }, 900)
+
+                // Display a new question after time delay
                 this.displayCard()
 
-            }, 2000)
-
+            }, 1000)
+            // this.setState({ active: true });
 
 
 
             // Answer is wrong
         } else if (card.correct_answer !== card[answer]) {
-            switch (answer) {
-                case 'answer_1':
+
+            // Turns the clicked "wrong" item red
+            this.setState({
+                selElement: 'box red red:hover'
+            })
+
+
+
+            // className={
+            //     this.state.ans4w ? 'box red red:hover' :
+            //         this.state.ans4 ? 'box green green:hover' : 'box'
+            // }
+            console.log('Correct Answer = ', card)
+            console.log('Card Answer 1= ', card.answer_1)
+            switch (card.correct_answer) {
+
+                case card.answer_1:
                     this.setState({ ans1w: true, ans2w: false, ans3w: false, ans4w: false })
                     break;
-                case 'answer_2':
+                case card.answer_2:
                     this.setState({ ans1w: false, ans2w: true, ans3w: false, ans4w: false })
                     break;
-                case 'answer_3':
+                case card.answer_3:
                     this.setState({ ans1w: false, ans2w: false, ans3w: true, ans4w: false })
                     break;
-                case 'answer_4':
+                case card.answer_4:
                     this.setState({ ans1w: false, ans2w: false, ans3w: false, ans4w: true })
                     break;
                 default:
                     break;
             }
+
             this.setState({
                 answerState: false,
                 answerText: 'Wrong',
                 // answerFlash: true
             })
             this.AnswerModal()
+
+
             setTimeout(() => {
                 this.setState({
                     removeModal: true,
@@ -177,13 +216,19 @@ class PlayArea extends Component {
 
                 // Flash correct answer
 
+                this.setState({ active: false });
+
+                // Wait, and then flip back to the next question
+                setTimeout(() => {
+                    this.setState({ active: true });
+                }, 900)
 
                 // flip card back
-                this.toggleClass()
+                // this.toggleClass()
 
                 // Displays a new card
                 this.displayCard()
-            }, 5000)
+            }, 4000)
 
         }
         // Next?
@@ -268,8 +313,10 @@ class PlayArea extends Component {
                         <div
                             className={this.state.active ? 'card is-flipped' : 'card'}
                             onClick={() => {
-                                this.respTimer('start')
+                                // Without this your card will not flip when you click it
                                 this.toggleClass()
+                                // Starts the response timer
+                                this.respTimer('start')
 
                             }}
                         >
@@ -280,35 +327,31 @@ class PlayArea extends Component {
                                 </div>
                                 <div
                                     className={
-                                        this.state.ans1 ? 'box green green:hover' :
-                                            this.state.ans1w ? 'box red red:hover' : 'box'
-                                        // this.state.answerFlash ? 'box flash' : 'box'
+                                        this.state.ans1 ? this.state.selElement :
+                                            this.state.ans1w ? 'flash box' : 'box'
                                     }
                                     onClick={() => this.AnswerClickHandler('answer_1')}
                                 >{this.state.card.answer_1}</div>
                                 <div
                                     className={
-                                        this.state.ans2 ? 'box green green:hover' :
-                                            this.state.ans2w ? 'box red red:hover' : 'box'
-                                        // this.state.answerFlash ? 'box flash' : 'box'
+                                        this.state.ans2 ? this.state.selElement :
+                                            this.state.ans2w ? 'flash box' : 'box'
                                     }
 
                                     onClick={() => this.AnswerClickHandler('answer_2')}
                                 >{this.state.card.answer_2}</div>
                                 <div
                                     className={
-                                        this.state.ans3 ? 'box green green:hover' :
-                                            this.state.ans3w ? 'box red red:hover' : 'box'
-                                        // this.state.answerFlash ? 'box flash' : 'box'
+                                        this.state.ans3 ? this.state.selElement :
+                                            this.state.ans3w ? 'flash box' : 'box'
                                     }
 
                                     onClick={() => this.AnswerClickHandler('answer_3')}
                                 >{this.state.card.answer_3}</div>
                                 <div
                                     className={
-                                        this.state.ans4 ? 'box green green:hover' :
-                                            this.state.ans4w ? 'box red red:hover' : 'box'
-                                        // this.state.answerFlash ? 'box flash' : 'box'
+                                        this.state.ans4 ? this.state.selElement :
+                                            this.state.ans4w ? 'flash box' : 'box'
                                     }
 
                                     onClick={() => this.AnswerClickHandler('answer_4')}
